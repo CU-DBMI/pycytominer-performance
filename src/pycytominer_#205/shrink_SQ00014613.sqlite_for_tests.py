d18876aebfdf54508e7c83eb85e6ee8653045c39
d33bs <dave.bunten@cuanschutz.edu>
2022-07-19 09:21:13 -0600

    Create shrink_SQ00014613.sqlite_for_tests.py

---
+++ src/pycytominer_#205/shrink_SQ00014613.sqlite_for_tests.py
@@ -0,0 +1,56 @@
+"""
+Creates small test dataset from SQ00014613.sqlite for testing
+for cytomining/pycytominer#205.
+
+Source:
+https://nih.figshare.com/articles/dataset/
+Cell_Health_-_Cell_Painting_Single_Cell_Profiles/9995672?file=18506036
+"""
+
+import shutil
+import sqlite3
+
+sqlite_source = "SQ00014613.sqlite"
+sqlite_target = "test_SQ00014613.sqlite"
+
+shutil.copy(sqlite_source, sqlite_target)
+
+sqlite_conn = sqlite3.connect(sqlite_target)
+
+delete_statement = """
+DELETE FROM Image 
+WHERE TableNumber NOT IN 
+('dd77885d07028e67dc9bcaaba4df34c6')
+"""
+sqlite_conn.execute(delete_statement)
+sqlite_conn.commit()
+
+delete_statement = """
+DELETE FROM Cytoplasm 
+WHERE TableNumber NOT IN (SELECT TableNumber FROM Image)
+OR ObjectNumber > 1
+"""
+sqlite_conn.execute(delete_statement)
+
+delete_statement = """
+DELETE FROM Nuclei 
+WHERE TableNumber NOT IN (SELECT TableNumber FROM Image)
+OR ObjectNumber > 1
+"""
+sqlite_conn.execute(delete_statement)
+
+delete_statement = """
+DELETE FROM Cells 
+WHERE TableNumber NOT IN (SELECT TableNumber FROM Image)
+OR ObjectNumber > 1
+"""
+sqlite_conn.execute(delete_statement)
+
+sqlite_conn.commit()
+
+sqlite_conn.execute("VACUUM")
+
+sqlite_conn.commit()
+
+# close connections
+sqlite_conn.close()
